5d86c844cd2e113ce95c398b61b000f194371ece
Juttacci <guy.hazoume@epitech.eu>
2025-07-30 13:29:13 +0100

    feat(player): add the basic player movement

---
+++ Assets/Bouncy/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float jumpForce = 5f;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    [System.Obsolete]
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (transform.position.y > 14f)
+        { 
+            transform.position = new Vector3(transform.position.x, 14f, transform.position.z);
+            playerRb.velocity = new Vector3(playerRb.velocity.x, 0f, playerRb.velocity.z); // Annule la vitesse verticale
+        }
+    }
+
+    void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
